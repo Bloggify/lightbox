a4f1ab0ba6d3f2bd0de4bd29f8ce49e3d6ddc1d5
Ionică Bizău <bizauionica@yahoo.com>
2014-11-03 18:17:52 +0200

    Preparing 2.0.0

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-exports.init = function () {
-    var self = this;
-    Config.options = Object(Config.options);
-
-    var customCSS = Config.options.customCSS = Config.options.customCSS || []
-      , customJS = Config.options.customJS  = Config.options.customJS  || []
-      ;
-
+module.exports = function () {
     // Add JS urls
-    customJS.push("/api/plugin-file/" + self.name + "/js/lightbox.js");
-    customJS.push("/api/plugin-file/" + self.name + "/js/lightbox-handlers.js");
+    Bloggify._config.customJS.push("/api/plugin-file/" + self.name + "/js/lightbox.js");
+    Bloggify._config.customJS.push("/api/plugin-file/" + self.name + "/js/lightbox-handlers.js");
 
-    // Add CSS urls
-    customCSS.push("/api/plugin-file/" + self.name + "/css/lightbox.css");
+    // Add CSS url
+    Bloggify._config.customCSS.push("/api/plugin-file/" + self.name + "/css/lightbox.css");
 };
