3653d8e44fbde048eca33ea57ea7c795a8c31ea9
Ionică Bizău <bizauionica@yahoo.com>
2014-11-03 18:20:18 +0200

    customCSS and customJS are properties of options object

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 module.exports = function (plugin) {
     // Add JS urls
-    Bloggify._config.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox.js");
-    Bloggify._config.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox-handlers.js");
+    Bloggify._config.options.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox.js");
+    Bloggify._config.options.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox-handlers.js");
 
     // Add CSS url
-    Bloggify._config.customCSS.push("/api/plugin-file/" + plugin.name + "/css/lightbox.css");
+    Bloggify._config.options.customCSS.push("/api/plugin-file/" + plugin.name + "/css/lightbox.css");
 };
