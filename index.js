5e571b2c60480e95cac5eae35d4940b339576646
Ionică Bizău <bizauionica@yahoo.com>
2015-07-09 09:26:57 +0300

    Fixed the arguments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-module.exports = function (plugin) {
+module.exports = function () {
     // Add JS urls
-    Bloggify.config.options.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox.js");
-    Bloggify.config.options.customJS.push("/api/plugin-file/" + plugin.name + "/js/lightbox-handlers.js");
+    Bloggify.config.options.customJS.push("/api/plugin-file/" + this.name + "/js/lightbox.js");
+    Bloggify.config.options.customJS.push("/api/plugin-file/" + this.name + "/js/lightbox-handlers.js");
 
     // Add CSS url
-    Bloggify.config.options.customCSS.push("/api/plugin-file/" + plugin.name + "/css/lightbox.css");
+    Bloggify.config.options.customCSS.push("/api/plugin-file/" + this.name + "/css/lightbox.css");
 };
